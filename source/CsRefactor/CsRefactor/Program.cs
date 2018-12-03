96f83e8df779572743e392f55dde8df5c8865f82
masaki_takahashi <masaki_takahashi@mdingon.com>
2018-12-03 18:09:59 +0900

    feat: とりあえずエントリポイントから適当に実行して結果をだした

--- source/CsRefactor/CsRefactor/Program.cs
+++ source/CsRefactor/CsRefactor/Program.cs
@@ -6,7 +6,20 @@ namespace CsRefactor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var もののけ姫 = new Movie("もののけ姫", Movie.REGULAR);
+            var アンパンマン = new Movie("アンパンマン", Movie.CHILDRENS);
+            var ボヘミアンラプソディ = new Movie("ボヘミアン・ラプソディ", Movie.NEW_RELEASE);
+
+            var オギー = new Customer("オギー");
+
+            オギー.AddRental(new Rental(アンパンマン, 3));
+            オギー.AddRental(new Rental(もののけ姫, 7));
+            オギー.AddRental(new Rental(ボヘミアンラプソディ, 1));
+
+            Console.WriteLine(オギー.Statement());
+
+            Console.Read();
+
         }
     }
 }
