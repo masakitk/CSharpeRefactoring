d22b77fe22b441db5ca031b6b39d0dc2a30a5cd2
masaki_takahashi <masaki_takahashi@mdingon.com>
2018-12-03 17:41:58 +0900

    feat: statement手前まで

---
+++ source/CsRefactor/CsRefactor/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace CsRefactor
+{
+    public class Customer
+    {
+        private string _name;
+        private List<Rental> _rentals;
+
+        public Customer(string name)
+        {
+            _name = name;
+        }
+
+        public void AddRental(Rental arg)
+        {
+            _rentals.Add(arg);
+        }
+
+        public string getName()
+        {
+            return _name;
+        }
+    }
+}
