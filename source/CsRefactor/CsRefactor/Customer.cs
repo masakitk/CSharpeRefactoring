465c968ef12d7f3def818875eeb64e0fe30849e8
masaki_takahashi <masaki_takahashi@mdingon.com>
2019-01-09 14:29:49 +0900

    refactor: 合計料金取得メソッドを切り出し

--- source/CsRefactor/CsRefactor/Customer.cs
+++ source/CsRefactor/CsRefactor/Customer.cs
@@ -21,7 +21,6 @@ namespace CsRefactor
 
         public string Statement()
         {
-            double totalAmount = 0;
             var frequentRenterPoints = 0;
             IEnumerable<Rental> rentals = _rentals;
             var result = "Rental Record for " + Name + Environment.NewLine;
@@ -31,12 +30,21 @@ namespace CsRefactor
                 //この貸出に関する数値の表示
                 result += "\t" + rental.Movie.Title+ "\t" +
                           rental.GetCharge() + Environment.NewLine;
-                totalAmount += rental.GetCharge();
             }
 
-            result += "Amount owed is " + totalAmount + Environment.NewLine;
+            result += $"Amount owed is {GetTotalAmount()}{Environment.NewLine}";
             result += "You earned " + frequentRenterPoints + " frequent renter points";
             return result;
+        }
+
+        private double GetTotalAmount()
+        {
+            double totalAmount = 0;
+            foreach (var rental in _rentals)
+            {
+                totalAmount += rental.GetCharge();
+            }
+            return totalAmount;
         }
     }
 }
